3335e045d83b79b6ee3c7e58a7536377417e9e80
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-07-31 23:33:15 +0300

    feat: add uinput module

---
+++ modules/hardware/uinput.nix
@@ -0,0 +1,18 @@
+# This module needed for creating and manage virtual input devices.
+# https://www.kernel.org/doc/html/v4.12/input/uinput.html
+{
+  config,
+  lib,
+  ...
+}:
+{
+  options.uinput = {
+    enable = lib.mkEnableOption "Enable virtual input module" // {
+      default = true;
+    };
+  };
+  config = lib.mkIf config.uinput.enable {
+    users.users."${config.var.username}".extraGroups = [ "input" ];
+    hardware.uinput.enable = true;
+  };
+}
