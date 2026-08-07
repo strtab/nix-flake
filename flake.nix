215df25db8eb43aef81af8298b869ae4ae98f83e
Maleshko Daniil <maleshkodaniil@gmail.com>
2026-08-07 18:49:45 +0300

    dotfiles: update source

--- flake.nix
+++ flake.nix
@@ -24,12 +24,9 @@
     geist.url = "github:strtab/Plasma-Geist-visual-style";
     geist.flake = false;
 
-    dotfiles.url = "gitlab:strtab/illogical-impulce-dots/f1f7707b813f767f9823e052d69b831e6aa55d4d";
-    dotfiles.flake = false;
-    illogical-flake = {
-      url = "gitlab:strtab/illogical-flake/910eba218abc31adab2e685233cd80e22e61b823";
+    aether = {
+      url = "github:strtab/aether";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.dotfiles.follows = "dotfiles";
     };
   };
 
